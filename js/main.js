19767ab274832060da5eacc15bf6c4a23fe4a31e
jayroguerrs <jayroguerreros@gmail.com>
2022-06-09 18:19:33 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,39 @@
+$(document).ready(function(){
+  $("div.falda").hide();
+  $("div.pantalon").hide();
+  $("div.eleccion").hide();
+  $("div.blusa").hide();
+  $("div.chaleco").hide();
+  
+  $("input[name=opt-genero]").click(function(){
+      if ($('input:radio[name=opt-genero]:checked').val()=="FEMENINO"){
+          $("div.eleccion").show();
+          $("div.blusa").show();
+      } else {
+          $("div.eleccion").hide();
+          $("div.blusa").hide();
+      }              
+  });
+
+  $("input[name=opt-preferencia]").click(function(){
+      if ($('input:radio[name=opt-preferencia]:checked').val()=="2 PANTALONES"){
+          $("div.pantalon").show();
+          $("div.falda").hide();
+      } else if ($('input:radio[name=opt-preferencia]:checked').val()=="2 FALDAS"){
+        $("div.pantalon").hide();
+        $("div.falda").show();
+      } else {
+        $("div.pantalon").show();
+        $("div.falda").show();
+      }            
+  });
+  $("#single").val("Single2").trigger('change'); 
+});
+
+
+
+
+
 $(function () {
     $('select').each(function () {
       $(this).select2({
@@ -12,5 +48,5 @@ $(function () {
     $('#rango').on("input", function() {
       $('.output').text(this.value +" cms" );
     }).trigger("change");
-});
+})
 
